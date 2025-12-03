de0dcac48c61fd836b4b73a88b8ebea38e1e7f7c
rosoics <rose_show@yahoo.com>
2025-12-03 10:25:32 +0700

    Dev FormulariumListQuery

---
+++ Farinv.Infrastructure/BrgContext/StandardFeature/FormulariumRepo.cs
@@ -0,0 +1,44 @@
+﻿using Farinv.Application.BrgContext.StandardFeature;
+using Farinv.Domain.BrgContext.StandardFeature;
+using Nuna.Lib.PatternHelper;
+
+namespace Farinv.Infrastructure.BrgContext.StandardFeature;
+
+public class FormulariumRepo : IFormulariumRepo
+{
+    private readonly IFormulariumDal _formulariumDal;
+
+    public FormulariumRepo(IFormulariumDal formulariumDal)
+    {
+        _formulariumDal = formulariumDal;
+    }
+
+    public void SaveChanges(FormulariumType model)
+    {
+        LoadEntity(model)
+            .Match(
+                onSome: _ => _formulariumDal.Update(FormulariumDto.FromModel(model)),
+                onNone: () => _formulariumDal.Insert(FormulariumDto.FromModel(model)));
+    }
+
+    public MayBe<FormulariumType> LoadEntity(IFormulariumKey key)
+    {
+        var dto = _formulariumDal.GetData(key);
+        if (dto is null)
+            return MayBe<FormulariumType>.None;
+        var model = dto.ToModel();
+        return MayBe.From(model);
+    }
+
+    public void DeleteEntity(IFormulariumKey key)
+    {
+        _formulariumDal.Delete(key);
+    }
+
+    public IEnumerable<FormulariumType> ListData()
+    {
+        var listDto = _formulariumDal.ListData()?.ToList() ?? [];
+        var result = listDto.Select(x => x.ToModel()).ToList();
+        return result;
+    }
+}
---
+++ Farinv.Sqldb/BrgContext/StandardFeature/tb_formularium.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE tb_formularium (
+    fs_kd_formularium VARCHAR(5) NOT NULL CONSTRAINT DF_tb_formularium_fs_kd_formularium DEFAULT(''),
+    fs_nm_formularium VARCHAR(30) NOT NULL CONSTRAINT DF_tb_formularium_fs_nm_formularium DEFAULT(''),
+    
+    CONSTRAINT PK_tb_formularium PRIMARY KEY CLUSTERED(fs_kd_formularium)
+)
