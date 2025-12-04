71bc8e04d01310876f4c8c93fcdfae1e20bbae9a
rosoics <rose_show@yahoo.com>
2025-12-04 10:11:20 +0700

    Dev OriginalListQuery

---
+++ Farinv.Domain/BrgContext/StandardFeature/OriginalType.cs
@@ -0,0 +1,35 @@
+﻿using Ardalis.GuardClauses;
+
+namespace Farinv.Domain.BrgContext.StandardFeature;
+
+public record OriginalType : IOriginalKey
+{
+    #region CREATION
+    public OriginalType(string originalId, string originalName)
+    {
+        OriginalId = originalId;
+        OriginalName = originalName;
+    }
+
+    public static OriginalType Create(string originalId, string originalName)
+    {
+        Guard.Against.NullOrWhiteSpace(originalId, nameof(originalId));
+        Guard.Against.NullOrWhiteSpace(originalName, nameof(originalName));
+        return new OriginalType(originalId, originalName);
+    }
+
+    public static OriginalType Default => new("-", "-");
+
+    public static IOriginalKey Key(string id) => Default with { OriginalId = id };
+    #endregion
+
+    #region PROPERTIES
+    public string OriginalId { get; init; }
+    public string OriginalName { get; init; }
+    #endregion
+}
+
+public interface IOriginalKey
+{
+    string OriginalId { get; }
+}
---
+++ Farinv.Infrastructure/BrgContext/StandardFeature/OriginalDto.cs
@@ -0,0 +1,16 @@
+﻿using Farinv.Domain.BrgContext.StandardFeature;
+
+namespace Farinv.Infrastructure.BrgContext.StandardFeature;
+
+public record OriginalDto(string fs_kd_original, string fs_nm_original)
+{
+    public static OriginalDto FromModel(OriginalType model)
+    {
+        return new OriginalDto(model.OriginalId, model.OriginalName);
+    }
+
+    public OriginalType ToModel()
+    {
+        return OriginalType.Create(fs_kd_original, fs_nm_original);
+    }
+}
