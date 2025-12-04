ed85cccaba732ced9e8a8046e570ddb97a0afb4f
rosoics <rose_show@yahoo.com>
2025-12-04 11:15:23 +0700

    Fix length

--- Farinv.Sqldb/BrgContext/StandardFeature/tb_gol_terapi.sql
+++ Farinv.Sqldb/BrgContext/StandardFeature/tb_gol_terapi.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE tb_gol_terapi
 (
-    fs_kd_gol_terapi   VARCHAR(20)  NOT NULL CONSTRAINT DF_tb_gol_terapi_fs_kd_gol_terapi DEFAULT(''),
-    fs_nm_gol_terapi   VARCHAR(100) NOT NULL CONSTRAINT DF_tb_gol_terapi_fs_nm_gol_terapi DEFAULT(''),
+    fs_kd_gol_terapi   VARCHAR(3)  NOT NULL CONSTRAINT DF_tb_gol_terapi_fs_kd_gol_terapi DEFAULT(''),
+    fs_nm_gol_terapi   VARCHAR(50) NOT NULL CONSTRAINT DF_tb_gol_terapi_fs_nm_gol_terapi DEFAULT(''),
     CONSTRAINT PK_tb_gol_terapi PRIMARY KEY CLUSTERED (fs_kd_gol_terapi)
 )
--- Farinv.Sqldb/BrgContext/StandardFeature/tb_original.sql
+++ Farinv.Sqldb/BrgContext/StandardFeature/tb_original.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE tb_original
 (
-    fs_kd_original   VARCHAR(20)  NOT NULL CONSTRAINT DF_tb_original_fs_kd_original DEFAULT(''),
-    fs_nm_original   VARCHAR(100) NOT NULL CONSTRAINT DF_tb_original_fs_nm_original DEFAULT(''),
+    fs_kd_original   VARCHAR(3)  NOT NULL CONSTRAINT DF_tb_original_fs_kd_original DEFAULT(''),
+    fs_nm_original   VARCHAR(30) NOT NULL CONSTRAINT DF_tb_original_fs_nm_original DEFAULT(''),
     CONSTRAINT PK_tb_original PRIMARY KEY CLUSTERED (fs_kd_original)
 )
