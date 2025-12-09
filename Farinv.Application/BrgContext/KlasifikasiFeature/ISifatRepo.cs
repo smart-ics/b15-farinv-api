83093ccf0508b007808b4410314a9e43babe8ed1
rosoics <rose_show@yahoo.com>
2025-12-09 14:29:26 +0700

    Dev SifatListQuery

---
+++ Farinv.Application/BrgContext/KlasifikasiFeature/ISifatRepo.cs
@@ -0,0 +1,12 @@
+﻿using Farinv.Domain.BrgContext.KlasifikasiFeature;
+using Nuna.Lib.DataAccessHelper;
+
+namespace Farinv.Application.BrgContext.KlasifikasiFeature;
+
+public interface ISifatRepo :
+    ISaveChange<SifatType>,
+    ILoadEntity<SifatType, ISifatKey>,
+    IDeleteEntity<ISifatKey>,
+    IListData<SifatType>
+{
+}
---
+++ Farinv.Infrastructure/BrgContext/KlasifikasiFeature/SifatDto.cs
@@ -0,0 +1,16 @@
+﻿using Farinv.Domain.BrgContext.KlasifikasiFeature;
+
+namespace Farinv.Infrastructure.BrgContext.KlasifikasiFeature;
+
+public record SifatDto(string fs_kd_sifat, string fs_nm_sifat)
+{
+    public static SifatDto FromModel(SifatType model)
+    {
+        return new SifatDto(model.SifatId, model.SifatName);
+    }
+
+    public SifatType ToModel()
+    {
+        return SifatType.Create(fs_kd_sifat, fs_nm_sifat);
+    }
+}
