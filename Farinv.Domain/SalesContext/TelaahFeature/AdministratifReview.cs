136376ebba0dd9597cd110f555a18ec3f20755bb
rosoics <rose_show@yahoo.com>
2026-02-24 15:45:40 +0700

    Dev TelaahModel

---
+++ Farinv.Domain/SalesContext/TelaahFeature/AdministratifReview.cs
@@ -0,0 +1,37 @@
+﻿namespace Farinv.Domain.SalesContext.TelaahFeature;
+
+public class AdministratifReview
+{
+    private readonly Dictionary<AdministratifCheckEnum, bool?> _items;
+
+    #region CREATION
+    public AdministratifReview()
+    {
+        _items = Enum
+            .GetValues<AdministratifCheckEnum>()
+            .ToDictionary(x => x, _ => (bool?)null);
+    }
+    #endregion
+
+    #region PROPERTIES
+    public bool IsComplete => _items.Values.All(x => x.HasValue);
+    public bool IsFullyCompliant => IsComplete && 
+        _items.Values.All(x => x == true);
+    public bool HasIssue => _items.Values.Any(x => x == false);
+    #endregion
+
+    #region BEHAVIOR
+    public void SetAppropriate(AdministratifCheckEnum item, bool isAppropriate)
+    {
+        _items[item] = isAppropriate;
+    }
+    #endregion
+}
+
+public enum AdministratifCheckEnum
+{
+    Dokter,
+    Pasien,
+    TanggalResep,
+    CareUnit
+}
---
+++ Farinv.Domain/SalesContext/TelaahFeature/MedicationReview.cs
@@ -0,0 +1,41 @@
+﻿using Farinv.Domain.BrgContext.BrgFeature;
+
+namespace Farinv.Domain.SalesContext.TelaahFeature;
+
+public class MedicationReview
+{
+    private bool? _isDoseAppropriate;
+    private bool? _isEtiketAppropriate;
+
+    #region CREATION
+    internal MedicationReview(BrgReff brg)
+    {
+        Brg = brg;
+    }
+    #endregion
+
+    #region PROPERTIES
+    public BrgReff Brg { get; private set; }
+    public bool IsComplete => _isDoseAppropriate.HasValue &&
+        _isEtiketAppropriate.HasValue;
+
+    public bool IsFullyCompliant => IsComplete &&
+        _isDoseAppropriate == true &&
+        _isEtiketAppropriate == true;
+
+    public bool HasIssue => _isDoseAppropriate == false ||
+        _isEtiketAppropriate == false;
+    #endregion
+
+    #region BEHAVIOR
+    public void SetDoseAppropriate(bool compliant)
+    {
+        _isDoseAppropriate = compliant;
+    }
+
+    public void SetEtiketAppropriate(bool compliant)
+    {
+        _isEtiketAppropriate = compliant;
+    }
+    #endregion
+}
